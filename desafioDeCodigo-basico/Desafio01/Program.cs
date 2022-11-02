9a223df9d6c2d2364ad369b0b56dd7e7a6c6e09d
murilojcavalcanti <josemurilo7@gmail.com>
2022-11-01 23:27:55 -0300

    Primeiro Commit

---
+++ desafioDeCodigo-basico/Desafio01/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Dado um número inteiro ( n ) , retorne a diferença entre o produto de seus dígitos e 
+a soma de seus dígitos.*/
+
+using System;
+Console.WriteLine("Digite um valor inteiro ");
+int inteiro = Convert.ToInt32(Console.ReadLine());
+int produto = 1, soma=0;
+while(inteiro>0){
+    int dezena = inteiro%10;
+    produto= produto * dezena;
+    soma= soma + dezena;
+    inteiro = inteiro/10;
+}
+
+Console.WriteLine(produto - soma);
+
+
+
+
+
+
+// int centena=0;
+// if(inteiro>100&&inteiro>1000)
+// {centena = inteiro /100;
+// }
+// Console.WriteLine(centena);
